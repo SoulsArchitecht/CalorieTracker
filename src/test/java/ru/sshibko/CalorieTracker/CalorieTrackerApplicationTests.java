94430af952d7975a3f2f5e82b2beffc29b4bfd08
Serge <sshibkojavadev@gmail.com>
2025-03-20 20:12:41 +0800

    init commit

---
+++ src/test/java/ru/sshibko/CalorieTracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sshibko.CalorieTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
